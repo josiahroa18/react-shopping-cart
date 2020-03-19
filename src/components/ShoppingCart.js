7387cd329406fc9b04d7ad339892e52a452c6ab6
Josiah Roa <josiahroa18@gmail.com>
2020-03-18 22:32:08 -0600

    Completed MVP

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CardContext } from '../contexts/CardContext';
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
+	const cart = useContext(CardContext);
+
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
